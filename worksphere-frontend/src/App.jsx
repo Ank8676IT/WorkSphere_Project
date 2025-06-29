fa9e7cd2740b09a13b306b78e5bcd473e864d039
Ank8676IT <ankitoraon696867@gmail.com>
2025-06-29 23:06:44 +0530

    Landing Page Part 1

--- worksphere-frontend/src/App.jsx
+++ worksphere-frontend/src/App.jsx
@@ -2,16 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar1 from './components/NavbarV1/navbar1'
+import LandingPage from './pages/LandingPage/landingPage'
+import Footer from './components/Footer/footer'
+import { Routes,Route } from 'react-router-dom'
+import SignUp from './pages/SignUp/signUp'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className='bg-gray-950 text-white'>
-        Welcome to WorkSphere!
-      </div>
-    </>
+    <div className='bg-gray-100 w-[100%] h-[100%] box-border'>
+      <Navbar1 />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 
