d13b41a847984120a9f3b43f6b1387b0a061beda
Ank8676IT <ankitoraon696867@gmail.com>
2025-07-03 09:04:29 +0530

    Backend Part II

--- worksphere-backend/index.js
+++ worksphere-backend/index.js
@@ -1,15 +1,20 @@
 require('./connection');
 const express = require('express');
 const app = express();
+require('dotenv').config({path:"./config.env"});
+const cookieParser = require('cookie-parser')
+
+const PORT = process.env.PORT || 4000;
 
 
+app.use(express.json());
+app.use(cookieParser());    
 
-app.get('/',(req,res)=>{
-    res.send({
-        message:"Congrats on running Backend Server running on PORT 4000"
-    })
-})
+const UserRoutes = require('./routes/user');
 
-app.listen(4000, ()=>{
-    console.log("Backend Server is running on port 4000")
+
+app.use('/api/auth', UserRoutes);
+
+app.listen(PORT, ()=>{
+    console.log("Backend Server is running on Port", PORT)
 })
--- worksphere-frontend/src/main.jsx
+++ worksphere-frontend/src/main.jsx
@@ -6,6 +6,8 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const GOOGLE_CLIENT_ID = '696928690338-6j7h8v1fpn9orhpsdpg2mkn74m1v3kur.apps.googleusercontent.com';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_APP_GOOGLE_AUTH_KEY}>
